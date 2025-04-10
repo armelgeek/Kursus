6327cb5f65027cb87b1dabdbf78a4d149ea3e4d9
Hajavololona Armellin <armelgeek5@gmail.com>
2025-04-10 08:35:16 +0300

    feat: get  user progress

--- drizzle/schema/index.ts
+++ drizzle/schema/index.ts
@@ -1,3 +1,14 @@
 
 export { users, sessions, accounts, verifications } from './auth';
 export { categories } from './categories';
+export {
+    courses,
+    coursesRelations,
+    units,
+    unitsRelations,
+    lessons,
+    userProgress,
+    lessonsRelations,
+    challenges,
+    challengesRelations,
+} from './schema'
---
+++ features/courses/get-user-progress.use-case.ts
@@ -0,0 +1,15 @@
+import { db } from "@/drizzle/db";
+import { userProgress } from "@/drizzle/schema";
+import { eq } from "drizzle-orm";
+import { cache } from "react";
+
+export const getUserProgress = cache(async (userId: string) => {
+    const data = await db.query.userProgress.findFirst({
+        where: eq(userProgress.userId, userId),
+        with: {
+            activeCourse: true,
+        },
+    });
+
+    return data;
+});
